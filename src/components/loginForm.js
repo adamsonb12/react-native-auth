0b6d793183f0d43371074e9da3d7ad211410e5a7
Brett Adamson <adamsonb12@gmail.com>
2018-01-01 19:24:20 -0800

    login works with firebase, loading spinner when request is made

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+import { Text } from 'react-native';
 
 class LoginForm extends Component {
     state = {
         email: '',
         password: '',
+        error: '',
+        loading: false
     };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '', loading: true });
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(() => {
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
+            });
+    }
+
+    onLoginFail() {
+        this.setState({ error: 'Authentication Failed', loading: false });
+    }
+
+    onLoginSuccess() {
+        this.setState({ email: '', password: '', loading: false, error: '' });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+        return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>;
+    }
 
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input
-                        label='Email:'
+                        label="Email:"
                         value={this.state.email}
                         onChangeText={email => this.setState({ email: email })}
-                        placeholder='user@gmail.com'
+                        placeholder="user@gmail.com"
                     />
                 </CardSection>
 
                 <CardSection>
                     <Input
-                        placeholder='password'
+                        placeholder="password"
                         value={this.state.password}
                         onChangeText={password => this.setState({ password })}
-                        label='Password:'
+                        label="Password:"
                         secureTextEntry={true}
                     />
                 </CardSection>
 
-                <CardSection>
-                    <Button>
-                        Log in
-                    </Button>
-                </CardSection>
+                <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
+                <CardSection>{this.renderButton()}</CardSection>
             </Card>
         );
     }
@@ -44,7 +79,12 @@ const styles = {
     textInputStyle: {
         height: 20,
         width: 100
+    },
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
-}
+};
 
 export default LoginForm;
